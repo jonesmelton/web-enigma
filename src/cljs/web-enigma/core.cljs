2358e2eb66534cfb3191590e0c9b6cd41f89d96f
Jones Melton <shavah@gmail.com>
2016-05-26 14:59:06 -0500

    Add material css

--- src/cljs/web-enigma/core.cljs
+++ src/cljs/web-enigma/core.cljs
@@ -7,12 +7,14 @@
 (defonce app-state (atom ""))
 
 (defn greeting []
-  [:div
-    [:h1 [:input#engima-input {
-                  :type "text"
-                  :value @app-state
-                  :placeholder "text to encode"
-                  :on-change #(reset! app-state (-> % .-target .-value))}]]
-    [:h2 "encoded text you made: " (enigma/encode @app-state)]])
+  [:div.container
+    [:div.card-panel
+      [:h1 [:input#engima-input {
+                    :type "text"
+                    :value @app-state
+                    :placeholder "text to encode"
+                    :on-change #(reset! app-state (-> % .-target .-value))}]]]
+    [:div.card-panel.deep-purple.lighten-2
+      [:h4 "encoded text: " [:p (enigma/encode @app-state)]]]])
 
 (r/render [greeting] (js/document.getElementById "app"))
